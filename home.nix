97c6e1189f2cc508f730b7a566effc626a3fad40
Adrian Bernhard <adrianbernhard@gmail.com>
2024-08-05 22:32:13 +0200

    added git to home-manager

--- home.nix
+++ home.nix
@@ -20,9 +20,10 @@
   home.packages = [
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
-    pkgs.hello
     pkgs.chromium 
-    pkgs.kitty
+    pkgs.nemo
+    pkgs.gimp
+    pkgs.git
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
@@ -75,4 +76,51 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+  
+  programs.alacritty = {
+    enable = true;
+    settings = {
+    	font.size = 8;
+        font.normal = {
+            family = "FiraCode Nerd Font";
+            style = "Regular";
+        };
+        colors = {
+            bright = {
+                black = "0x928374";
+                blue = "0x076678";
+                cyan = "0x427b58";
+                green = "0x79740e";
+                magenta = "0x8f3f71";
+                red = "0x9d0006";
+                white = "0x3c3836";
+                yellow = "0xb57614";
+            };
+            normal = {
+                black = "0xfbf1c7";
+                blue = "0x458588";
+                cyan = "0x689d6a";
+                green = "0x98971a";
+                magenta = "0xb16286";
+                red = "0xcc241d";
+                white = "0x7c6f64";
+                yellow = "0xd79921";
+            }; 
+           primary = {
+                background = "0xf2e5bc";
+                foreground = "0x000000";
+           };  
+        };
+    };
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "Adrian Bernhard";
+    userEmail = "adrianbernhard@gmail.com";
+    extraConfig = {
+        init.defaultBranch = "main";
+    };
+  };
+
 }
